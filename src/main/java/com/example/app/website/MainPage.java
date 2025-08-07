6f45c33488ca0ce5aab60bab8b3bac48d7159ac9
StKucharski <stanislawkucharski205@gmail.com>
2025-08-07 16:33:59 +0200

    My little prep task + .mvnw dir fix + springdevtools?

--- src/main/java/com/example/app/website/MainPage.java
+++ src/main/java/com/example/app/website/MainPage.java
@@ -12,4 +12,13 @@ public class MainPage {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+    @GetMapping("/strona_stacha")
+        public String adder(
+          @RequestParam(value = "number1", defaultValue = "3") int number1,
+          @RequestParam(value = "number2", defaultValue = "7") int number2
+        ){
+		double output_nr = 1.3212412251 * number1 + 1.000421521 * number2;
+		return String.format("Hello Is %f your favourite number? Don't asnwer, I know it is", output_nr);
+        }
 }
